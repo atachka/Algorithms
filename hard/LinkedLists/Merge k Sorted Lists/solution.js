b505017ea4df6994cb91acf33e534657e794fa7e
atachka <56038697+atachka@users.noreply.github.com>
2023-03-13 15:07:51 +0400

    Merge k Sorted Lists Refactored Solution

--- hard/LinkedLists/Merge k Sorted Lists/solution.js
+++ hard/LinkedLists/Merge k Sorted Lists/solution.js
@@ -2,27 +2,30 @@ let mergeKLists = (list) => {
   if (list.length === 0) {
     return null;
   }
-  while (list.length > 1) {
-    let a = list.shift();
-    let b = list.shift();
+  let i = 0;
+  while (true) {
+    let a = list[i];
+    let b = list[i + 1];
     let mergedAB = mergeList(a, b);
+    if (list[i] === undefined || list[i + 1] === undefined) break;
     list.push(mergedAB);
+    i += 2;
   }
-  return list[0];
+  return list[list.length - 1] || null;
 };
 
 let mergeList = (a, b) => {
-  let newList = new ListNode();
+  let newList = new ListNode(0);
   let current = newList;
-
-  while (a !== null && b !== null) {
+  while (a && b) {
     if (a.val > b.val) {
-      current.next = a;
+      current.next = b;
       b = b.next;
     } else {
-      current.next = b;
+      current.next = a;
       a = a.next;
     }
+    current = current.next;
   }
   current.next = a || b;
 
